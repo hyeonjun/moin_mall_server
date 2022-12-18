7e0adb8a1e0b0a3d1c5e2a144bdaf5894f2d09a7
hjju <hjju@fin2b.com>
2022-12-18 15:23:24 +0900

    사용자 권한 단일

--- back/src/main/java/com/buying/back/application/account/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.buying.back.application.account.repository;
-
-import com.buying.back.application.account.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long>, RoleRepositoryCustom {
-
-}
