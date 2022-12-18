955207129dd043728d58fa07c9ffa3312ca9e59f
hjju <hjju@fin2b.com>
2022-12-18 17:13:30 +0900

    사용자 관련 Exception 패키지 변경

--- back/src/main/java/com/buying/back/application/account/code/exception/AccountException.java
+++ back/src/main/java/com/buying/back/application/account/code/exception/AccountException.java
@@ -1,4 +1,4 @@
-package com.buying.back.application.account.code;
+package com.buying.back.application.account.code.exception;
 
 import com.buying.back.application.common.exception.CommonException;
 import com.buying.back.util.response.ResponseCode;
