cd5d7d152ead17ee681f3b29d262c4df36cb8c12
hjju <hjju@fin2b.com>
2022-12-18 17:14:40 +0900

    DTO의 Length Validation에 사용할 Util

---
+++ back/src/main/java/com/buying/back/util/verify/VerifyLengthUtil.java
@@ -0,0 +1,11 @@
+package com.buying.back.util.verify;
+
+public class VerifyLengthUtil {
+
+  public static final int MAX_DEFAULT_LENGTH = 150;
+  public static final int MAX_EMAIL_LENGTH = 191;
+  public static final int MAX_NAME_LENGTH = 191;
+  public static final int MAX_PASSWORD_LENGTH = 100;
+
+
+}
