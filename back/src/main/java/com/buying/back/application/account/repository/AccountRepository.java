b064a433fd39b549cd76f05302d155ef764076f7
hjju <hjju@fin2b.com>
2022-12-18 14:27:45 +0900

    사용자 JpaRepository

---
+++ back/src/main/java/com/buying/back/application/account/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.buying.back.application.account.repository;
+
+import com.buying.back.application.account.domain.Account;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+  Optional<Account> findByEmail(String email);
+
+}
