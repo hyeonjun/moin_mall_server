069a06324b5a83b777ccc010c4467131a57d041d
hjju <hjju@fin2b.com>
2023-01-01 23:03:09 +0900

    email type 중 reset password subject 수정

--- back/src/main/java/com/buying/back/util/email/HtmlEmailType.java
+++ back/src/main/java/com/buying/back/util/email/HtmlEmailType.java
@@ -5,7 +5,7 @@ import com.buying.back.util.html.HtmlType;
 public enum HtmlEmailType implements HtmlType {
 
   SAMPLE("email/sample", "Sample"),
-  RESET_PASSWORD("email/ResetPassword", "Reset Password");
+  RESET_PASSWORD("email/ResetPassword", "[BYEBUYING] %s 고객님 임시비밀번호가 발급되었습니다.");
 
   private final String templatePath;
   private final String subject;
