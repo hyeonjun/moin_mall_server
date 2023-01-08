7039d2a7637c3f49d19bc4fe30183a102b343153
hjju <hjju@fin2b.com>
2023-01-08 19:02:08 +0900

    설명 관련 길이 제한 추가

--- back/src/main/java/com/buying/back/util/verify/VerifyLengthUtil.java
+++ back/src/main/java/com/buying/back/util/verify/VerifyLengthUtil.java
@@ -6,6 +6,7 @@ public class VerifyLengthUtil {
   public static final int MAX_EMAIL_LENGTH = 191;
   public static final int MAX_NAME_LENGTH = 191;
   public static final int MAX_PASSWORD_LENGTH = 100;
+  public static final int MAX_DESCRIPTION_LENGTH = 1000;
 
 
 }
