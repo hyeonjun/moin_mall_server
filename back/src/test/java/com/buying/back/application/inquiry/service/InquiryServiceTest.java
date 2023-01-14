2adab07c079e56605933c47c31c373dba5b82649
hjju <hjju@fin2b.com>
2023-01-15 00:27:42 +0900

    문의사항 페이징 조회 쿼리 테스트 및 서비스 테스트

--- back/src/test/java/com/buying/back/application/inquiry/service/InquiryServiceTest.java
+++ back/src/test/java/com/buying/back/application/inquiry/service/InquiryServiceTest.java
@@ -1,18 +1,25 @@
 package com.buying.back.application.inquiry.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import com.buying.back.application.account.domain.Account;
 import com.buying.back.application.account.repository.AccountRepository;
+import com.buying.back.application.common.dto.PagingDTO;
 import com.buying.back.application.inquiry.code.exception.InquiryException;
 import com.buying.back.application.inquiry.controller.dto.CreateInquiryDTO;
 import com.buying.back.application.inquiry.repository.InquiryRepository;
 import com.buying.back.application.inquiry.service.vo.InquiryVO;
 import com.buying.back.application.mock.inquiry.InquiryMockDTO;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,19 +28,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("local")
 @ExtendWith(MockitoExtension.class)
 class InquiryServiceTest {
 
-  @Spy
   @InjectMocks
   private InquiryService inquiryService;
 
-  @Mock
+  @Spy
   private InquiryRepository inquiryRepository;
-  @Mock
+  @Spy
   private AccountRepository accountRepository;
 
   @DisplayName("일반 문의사항 생성 - 성공")
@@ -45,6 +53,8 @@ class InquiryServiceTest {
     given(accountRepository.findById(anyLong())).willReturn(Optional.ofNullable(account));
 
     InquiryVO vo = inquiryService.createInquiry(1L, dto);
+
+    verify(inquiryRepository).save(any());
 
     assertEquals(dto.getTitle(), vo.getTitle());
     assertEquals(dto.getContent(), vo.getContent());
@@ -64,5 +74,22 @@ class InquiryServiceTest {
     assertThrows(InquiryException.class, () -> {
       inquiryService.createInquiry(1L, dto);
     });
+  }
+
+  @DisplayName("일반 문의사항 페이징 조회")
+  @Test
+  void getInquiryByAccount() {
+    PagingDTO dto = new PagingDTO();
+
+    Account account = mock(Account.class);
+    given(accountRepository.findById(anyLong())).willReturn(Optional.ofNullable(account));
+    given(inquiryRepository.findAllByAccount(any(), anyLong()))
+      .willAnswer(invocation -> {
+        List<InquiryVO> list = List.of(new InquiryVO());
+        return new PageImpl<>(list);
+    });
+
+    Page<InquiryVO> vo = inquiryService.getInquiryByAccount(1L, dto);
+    assertNotNull(vo);
   }
 }
