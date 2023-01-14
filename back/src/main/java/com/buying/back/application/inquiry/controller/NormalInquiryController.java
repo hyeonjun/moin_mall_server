a73ff66eb29759555222e0e7e73ea82fb1d6610e
hjju <hjju@fin2b.com>
2023-01-15 00:29:37 +0900

    문의사항 페이징 조회 API

--- back/src/main/java/com/buying/back/application/inquiry/controller/NormalInquiryController.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/NormalInquiryController.java
@@ -2,6 +2,7 @@ package com.buying.back.application.inquiry.controller;
 
 import static com.buying.back.util.response.CommonResponseCode.SUCCESS;
 
+import com.buying.back.application.common.dto.PagingDTO;
 import com.buying.back.application.inquiry.controller.dto.CreateInquiryDTO;
 import com.buying.back.application.inquiry.service.InquiryService;
 import com.buying.back.application.inquiry.service.vo.InquiryVO;
@@ -9,7 +10,9 @@ import com.buying.back.infra.config.security.loginuser.LoginUser;
 import com.buying.back.util.response.CommonResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,10 +25,18 @@ public class NormalInquiryController {
 
   private final InquiryService inquiryService;
 
-  @PostMapping()
+  @PostMapping
   public CommonResponse<InquiryVO> createInquiry(@AuthenticationPrincipal LoginUser loginUser,
     @Valid @RequestBody CreateInquiryDTO dto) {
     InquiryVO vo = inquiryService.createInquiry(loginUser.getId(), dto);
+    return new CommonResponse<>(vo, SUCCESS);
+  }
+
+  @GetMapping
+  public CommonResponse<Page<InquiryVO>> getInquiryByAccount(
+    @AuthenticationPrincipal LoginUser loginUser,
+    @Valid @RequestBody PagingDTO dto) {
+    Page<InquiryVO> vo = inquiryService.getInquiryByAccount(loginUser.getId(), dto);
     return new CommonResponse<>(vo, SUCCESS);
   }
 
