ed953ddf01c10042404389c33a5490d4af3dedbf
hjju <hjju@fin2b.com>
2023-01-15 18:19:16 +0900

    불필요한 Import 삭제

--- back/src/test/java/com/buying/back/application/inquiry/contoller/dto/InquiryDtoObjectMapperTest.java
+++ back/src/test/java/com/buying/back/application/inquiry/contoller/dto/InquiryDtoObjectMapperTest.java
@@ -14,7 +14,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest(classes = ObjectMapperConfig.class)
 public class InquiryDtoObjectMapperTest {
