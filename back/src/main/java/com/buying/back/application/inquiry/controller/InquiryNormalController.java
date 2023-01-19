3765c43dcf0646dccb98586aa8c5c5860c379c7b
hjju <hjju@fin2b.com>
2023-01-19 14:30:18 +0900

    DeleteMapping -> PutMapping

--- back/src/main/java/com/buying/back/application/inquiry/controller/InquiryNormalController.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/InquiryNormalController.java
@@ -14,7 +14,6 @@ import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,7 +60,7 @@ public class InquiryNormalController {
     return new CommonResponse<>(vo, SUCCESS);
   }
 
-  @DeleteMapping("/{inquiry-id}")
+  @PutMapping("/{inquiry-id}")
   public CommonResponse<Void> deleteMyInquiry(@AuthenticationPrincipal LoginUser loginUser,
     @PathVariable(value = "inquiry-id") Long inquiryId) {
     inquiryService.deleteMyInquiry(loginUser.getId(), inquiryId);
