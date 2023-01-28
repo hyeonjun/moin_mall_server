9c483a2d68513d149fa76e17588efc9a008b6679
hjju <hjju@fin2b.com>
2023-01-29 00:09:28 +0900

    RedisConfig에 redisTemplate 추가, Serialize

--- back/src/main/java/com/buying/back/infra/config/RedisConfig.java
+++ back/src/main/java/com/buying/back/infra/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.buying.back.infra.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Profile({"dev", "prd"})
@@ -18,5 +23,22 @@ public class RedisConfig {
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
     return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    redisTemplate.setEnableTransactionSupport(true);
+
+    RedisSerializer<String> stringRedisSerializer = new StringRedisSerializer();
+    redisTemplate.setHashValueSerializer(stringRedisSerializer);
+    redisTemplate.setKeySerializer(stringRedisSerializer);
+    redisTemplate.setValueSerializer(stringRedisSerializer);
+    redisTemplate.setHashKeySerializer(stringRedisSerializer);
+
+    redisTemplate.afterPropertiesSet();
+
+    return redisTemplate;
   }
 }
