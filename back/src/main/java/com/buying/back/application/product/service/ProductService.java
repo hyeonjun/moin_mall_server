2b3c50f4fa58515e293d4ef2de3d7c61450b2e81
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2023-01-30 17:03:39 +0900

    add: ProductService
     create() 추가
    
     todo 테스트 중

---
+++ back/src/main/java/com/buying/back/application/product/service/ProductService.java
@@ -0,0 +1,69 @@
+package com.buying.back.application.product.service;
+
+import com.buying.back.application.product.controller.dto.ItemDto;
+import com.buying.back.application.product.controller.dto.OptionDto;
+import com.buying.back.application.product.controller.dto.ProductDto;
+import com.buying.back.application.product.domain.Item;
+import com.buying.back.application.product.domain.Option;
+import com.buying.back.application.product.domain.Product;
+import com.buying.back.application.product.repository.ItemRepository;
+import com.buying.back.application.product.repository.OptionRepository;
+import com.buying.back.application.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.loadtime.Options;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+    private final ItemRepository itemRepository;
+    private final OptionRepository optionRepository;
+
+    @Transactional
+    public Product createProduct(ProductDto.Create dto) {
+        List<ItemDto.Create> itemsDto = dto.getItemsDto();
+
+        Product product = Product.create(dto);
+        productRepository.save(product);
+
+        /*List<Option> options = optionsDto.stream().map(optionDto -> {
+            Option option = Option.create(optionDto);
+            option.setProduct(product);
+            return option;
+        }).collect(Collectors.toList());
+        optionRepository.saveAll(options);*/
+
+        List<Item> items = itemsDto.stream().map(itemDto -> {
+            StringBuilder itemName = new StringBuilder();
+            StringBuilder itemOptions = new StringBuilder();
+
+            itemName.append(product.getName()).append("/");
+
+            List<Option> options = itemDto.getOptionsDto().stream().map(info -> {
+                itemName.append(info.getOptionValue())
+                        .append("/");
+                return Option.create(info);
+            }).collect(Collectors.toList());
+
+            optionRepository.saveAll(options);
+            options.forEach(option -> itemOptions.append(option.getId()).append("/"));
+
+            itemDto.setName(itemName.toString());
+            itemDto.setOptions(itemOptions.toString());
+
+            Item item = Item.create(itemDto);
+            item.setProduct(product);
+            return item;
+        }).collect(Collectors.toList());
+        itemRepository.saveAll(items);
+
+        return product;
+    }
+}
+
