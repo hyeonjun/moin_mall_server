055f9de502a449ebe0064976b2d1136e05eccda5
hjju <hjju@fin2b.com>
2023-01-31 14:08:15 +0900

    문의사항 관리자 페이징 조회 API

--- back/src/main/java/com/buying/back/application/inquiry/controller/InquiryManagementController.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/InquiryManagementController.java
@@ -1,15 +1,23 @@
 package com.buying.back.application.inquiry.controller;
 
-import com.buying.back.application.inquiry.controller.dto.ReplyInquiryDTO;
+import com.buying.back.application.inquiry.code.type.InquiryParentType;
+import com.buying.back.application.inquiry.controller.dto.management.ReplyInquiryManagementDTO;
+import com.buying.back.application.inquiry.controller.dto.management.SearchInquiryManagementDTO;
 import com.buying.back.application.inquiry.service.InquiryService;
 import com.buying.back.application.inquiry.service.vo.InquiryDetailVO;
+import com.buying.back.application.inquiry.service.vo.InquiryManagementVO;
+import com.buying.back.application.inquiry.service.vo.InquiryVO;
 import com.buying.back.util.response.CommonResponse;
 import com.buying.back.util.response.CommonResponseCode;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +29,15 @@ public class InquiryManagementController {
 
   @PutMapping("/reply")
   public CommonResponse<InquiryDetailVO> replyToInquiry(
-    @Valid @RequestBody ReplyInquiryDTO dto) {
+    @Valid @RequestBody ReplyInquiryManagementDTO dto) {
     InquiryDetailVO vo = inquiryService.replyToInquiry(dto);
+    return new CommonResponse<>(vo, CommonResponseCode.SUCCESS);
+  }
+
+  @GetMapping
+  public CommonResponse<Page<InquiryManagementVO>> getInquiryList(
+    @Valid @RequestParam InquiryParentType dto) {
+    Page<InquiryManagementVO> vo = null; // inquiryService.getInquiryList(dto);
     return new CommonResponse<>(vo, CommonResponseCode.SUCCESS);
   }
 
