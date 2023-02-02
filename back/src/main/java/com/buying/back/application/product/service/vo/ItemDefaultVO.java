efafc370fb8796d8efecfe4e469a1b2ea50c5070
qnddjKJH <qnddjkjh.dev@gmail.com>
2023-02-03 01:35:00 +0900

    Modify: Response VO
     기본 응답 VO 필드 추가

--- back/src/main/java/com/buying/back/application/product/service/vo/ItemDefaultVO.java
+++ back/src/main/java/com/buying/back/application/product/service/vo/ItemDefaultVO.java
@@ -1,4 +1,23 @@
 package com.buying.back.application.product.service.vo;
 
+import java.util.List;
+
 public class ItemDefaultVO {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer quantity;
+    private Integer discountPrice;
+    private Integer discountRate;
+    private List<ItemOptionVO> options;
+
+    public ItemDefaultVO(Long id, String name, Integer price, Integer quantity, Integer discountPrice, Integer discountRate, List<ItemOptionVO> options) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.discountPrice = discountPrice;
+        this.discountRate = discountRate;
+        this.options = options;
+    }
 }
--- back/src/main/java/com/buying/back/application/product/service/vo/OptionDefaultVO.java
+++ back/src/main/java/com/buying/back/application/product/service/vo/OptionDefaultVO.java
@@ -1,4 +1,11 @@
 package com.buying.back.application.product.service.vo;
 
 public class OptionDefaultVO {
+    private Long id;
+    private String name;
+
+    public OptionDefaultVO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
