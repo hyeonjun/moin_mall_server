1d90d58bcda2544d0b6624a768f6c06d1f5fbcfe
hjju <hjju@fin2b.com>
2023-02-05 01:34:13 +0900

    문의사항 페이징 조회 API

--- back/src/main/java/com/buying/back/application/inquiry/controller/InquiryManagementController.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/InquiryManagementController.java
@@ -1,23 +1,19 @@
 package com.buying.back.application.inquiry.controller;
 
-import com.buying.back.application.inquiry.code.type.InquiryParentType;
 import com.buying.back.application.inquiry.controller.dto.management.ReplyInquiryManagementDTO;
 import com.buying.back.application.inquiry.controller.dto.management.SearchInquiryManagementDTO;
 import com.buying.back.application.inquiry.service.InquiryService;
 import com.buying.back.application.inquiry.service.vo.InquiryDetailVO;
 import com.buying.back.application.inquiry.service.vo.InquiryManagementVO;
-import com.buying.back.application.inquiry.service.vo.InquiryVO;
 import com.buying.back.util.response.CommonResponse;
 import com.buying.back.util.response.CommonResponseCode;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -36,9 +32,9 @@ public class InquiryManagementController {
 
   @GetMapping
   public CommonResponse<Page<InquiryManagementVO>> getInquiryList(
-    @Valid @RequestParam InquiryParentType dto) {
-    Page<InquiryManagementVO> vo = null; // inquiryService.getInquiryList(dto);
-    return new CommonResponse<>(vo, CommonResponseCode.SUCCESS);
+    @Valid SearchInquiryManagementDTO dto) {
+    Page<InquiryManagementVO> page = inquiryService.getInquiryList(dto);
+    return new CommonResponse<>(page, CommonResponseCode.SUCCESS);
   }
 
 }
