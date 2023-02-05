4b6b3ff8fcfb16a1b5ee4acd4189e2a2dc0c0a51
hjju <hjju@fin2b.com>
2023-02-05 14:55:03 +0900

    공통 DTO

--- back/src/main/java/com/buying/back/application/inquiry/controller/dto/management/SearchInquiryManagementDTO.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/dto/management/SearchInquiryManagementDTO.java
@@ -1,40 +1,15 @@
 package com.buying.back.application.inquiry.controller.dto.management;
 
-import com.buying.back.application.common.dto.PagingDTO;
-import com.buying.back.application.inquiry.code.type.InquiryChildType;
-import com.buying.back.application.inquiry.code.type.InquiryParentType;
-import com.buying.back.util.validation.validatedto.ValidateDTO;
-import com.buying.back.util.validation.validatedto.inquiry.InquiryDTOValidationCondition;
-import java.time.LocalDate;
+import com.buying.back.application.inquiry.controller.dto.common.SearchInquiryDTO;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
-import org.springframework.validation.Errors;
 
 @Getter
 @Setter
-public class SearchInquiryManagementDTO extends PagingDTO implements ValidateDTO {
+public class SearchInquiryManagementDTO extends SearchInquiryDTO {
 
-  @DateTimeFormat(iso = ISO.DATE)
-  private LocalDate createdDateFrom;
-
-  @DateTimeFormat(iso = ISO.DATE)
-  private LocalDate createdDateTo;
-
-  private Boolean deleted;
   private Boolean replied;
-
-  private InquiryParentType inquiryParentType;
-  private InquiryChildType inquiryChildType;
 
   private String authorEmail;
 
-  @Override
-  public void validate(Errors e) {
-    InquiryDTOValidationCondition.childTypeNeedParentType(
-      e, this.inquiryParentType, this.inquiryChildType);
-    InquiryDTOValidationCondition.parentChildTypeMatch(
-      e, this.inquiryParentType, this.inquiryChildType);
-  }
 }
--- back/src/main/java/com/buying/back/application/inquiry/controller/dto/normal/SearchInquiryNormalDTO.java
+++ back/src/main/java/com/buying/back/application/inquiry/controller/dto/normal/SearchInquiryNormalDTO.java
@@ -1,37 +1,11 @@
 package com.buying.back.application.inquiry.controller.dto.normal;
 
-import com.buying.back.application.common.dto.PagingDTO;
-import com.buying.back.application.inquiry.code.type.InquiryChildType;
-import com.buying.back.application.inquiry.code.type.InquiryParentType;
-import com.buying.back.util.validation.validatedto.ValidateDTO;
-import com.buying.back.util.validation.validatedto.inquiry.InquiryDTOValidationCondition;
-import java.time.LocalDate;
+import com.buying.back.application.inquiry.controller.dto.common.SearchInquiryDTO;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
-import org.springframework.validation.Errors;
 
 @Getter
 @Setter
-public class SearchInquiryNormalDTO extends PagingDTO implements ValidateDTO {
+public class SearchInquiryNormalDTO extends SearchInquiryDTO {
 
-  @DateTimeFormat(iso = ISO.DATE)
-  private LocalDate createdDateFrom;
-
-  @DateTimeFormat(iso = ISO.DATE)
-  private LocalDate createdDateTo;
-
-  private Boolean replied;
-
-  private InquiryParentType inquiryParentType;
-  private InquiryChildType inquiryChildType;
-
-  @Override
-  public void validate(Errors e) {
-    InquiryDTOValidationCondition.childTypeNeedParentType(
-      e, this.inquiryParentType, this.inquiryChildType);
-    InquiryDTOValidationCondition.parentChildTypeMatch(
-      e, this.inquiryParentType, this.inquiryChildType);
-  }
 }
