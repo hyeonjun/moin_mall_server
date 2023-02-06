e1c6daa3de189a89e4794bf4119b7aa59874e802
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2023-02-06 17:42:48 +0900

    modify: ItemStatus

--- back/src/main/java/com/buying/back/application/product/code/ItemStatus.java
+++ back/src/main/java/com/buying/back/application/product/code/ItemStatus.java
@@ -1,14 +1,15 @@
 package com.buying.back.application.product.code;
 
-public enum StockStatus {
-    NORMAL("정상"),
+public enum ItemStatus {
+    SALE("판매"),
+    SALES_END("판매종료"),
     DEADLINE("품절임박"),
     SOLD_OUT("품절"),
     ;
 
     private final String status;
 
-    StockStatus(String status) {
+    ItemStatus(String status) {
         this.status = status;
     }
 
