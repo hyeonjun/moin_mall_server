fc41e62bc195523bb91cb5cb4797e75a2d44e77c
hjju <hjju@fin2b.com>
2023-02-18 15:27:32 +0900

    cors

--- back/src/main/java/com/buying/back/infra/config/webconfig/CorsConfig.java
+++ back/src/main/java/com/buying/back/infra/config/webconfig/CorsConfig.java
@@ -1,17 +1,20 @@
 package com.buying.back.infra.config.webconfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Profile({"local, dev"})
+@Slf4j
 @Configuration
+@Profile({"local", "dev"})
 public class CorsConfig {
 
   @Bean
   public WebMvcConfigurer corsConfigurer() {
+    log.info("Cors Config");
     return new WebMvcConfigurer() {
 
       @Override
