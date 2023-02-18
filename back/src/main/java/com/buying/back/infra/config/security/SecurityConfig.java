726fef0c483ead1fe5245746ecdd584e086d8fd5
hjju <hjju@fin2b.com>
2023-02-18 14:52:41 +0900

    cors

--- back/src/main/java/com/buying/back/infra/config/security/SecurityConfig.java
+++ back/src/main/java/com/buying/back/infra/config/security/SecurityConfig.java
@@ -123,7 +123,7 @@ public class SecurityConfig {
     return filter;
   }
 
-  @Profile(value = {"local", "dev"})
+  @Profile({"local", "dev"})
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
