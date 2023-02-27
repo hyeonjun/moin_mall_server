51015cf0923a9984f1c43c11a461fc0fd0e14b58
hjju <hjju@fin2b.com>
2023-02-27 16:19:16 +0900

    쿠폰 정보 수정, 검색 DTO

---
+++ back/src/main/java/com/buying/back/application/coupon/controller/dto/SearchCouponDTO.java
@@ -0,0 +1,18 @@
+package com.buying.back.application.coupon.controller.dto;
+
+import com.buying.back.application.common.dto.PagingDTO;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Getter
+@Setter
+public class SearchCouponDTO extends PagingDTO {
+
+  private Boolean activated;
+  @DateTimeFormat(iso = ISO.DATE)
+  private LocalDate expirationDate;
+
+}
