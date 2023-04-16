e9bf91ff46295eba209a5eacbd475216e53c87e5
hjju <hjju@fin2b.com>
2023-04-16 14:15:33 +0900

    상품 단건 조회

--- back/src/main/java/com/buying/back/application/product/repository/ProductRepositoryCustom.java
+++ back/src/main/java/com/buying/back/application/product/repository/ProductRepositoryCustom.java
@@ -1,11 +1,10 @@
 package com.buying.back.application.product.repository;
 
-import com.buying.back.application.account.domain.Brand;
 import com.buying.back.application.product.repository.param.SearchProductListParam;
-import com.buying.back.application.product.service.vo.ProductDefaultVO;
+import com.buying.back.application.product.service.vo.ProductVO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ProductRepositoryCustom {
-  Page<ProductDefaultVO> findAllByBrand(Pageable pageable, SearchProductListParam param);
+  Page<ProductVO> findAllByBrand(Pageable pageable, SearchProductListParam param);
 }
--- back/src/main/java/com/buying/back/application/product/service/vo/ProductItemVO.java
+++ back/src/main/java/com/buying/back/application/product/service/vo/ProductItemVO.java
@@ -10,16 +10,19 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
-public class ProductDetailVO extends ProductDefaultVO {
+public class ProductItemVO extends ProductVO {
+  
   private List<ItemVO> items;
 
-  private ProductDetailVO(Product product) {
+  private ProductItemVO(Product product) {
     super(product);
   }
 
-  public static ProductDetailVO valueOf(Product product, List<ItemVO> items) {
-    ProductDetailVO vo = new ProductDetailVO(product);
+  public static ProductItemVO valueOf(Product product, List<ItemVO> items) {
+    ProductItemVO vo = new ProductItemVO(product);
+
     vo.setItems(items);
+
     return vo;
   }
 }
